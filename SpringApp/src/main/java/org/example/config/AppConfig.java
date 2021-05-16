6ac8ad76e8e5dd14fefb38c4abcb60af9d15e244
LentCrown <lickly@yandex.ru>
2021-05-16 22:12:08 +0300

    changed variable name

--- SpringApp/src/main/java/org/example/config/AppConfig.java
+++ SpringApp/src/main/java/org/example/config/AppConfig.java
@@ -2,13 +2,12 @@ package org.example.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
 @ConfigurationProperties(prefix = "application")
 public class AppConfig {
     private String source;
     private char separator;
-    private boolean ignore_quatations;
+    private boolean ignore_quotations;
     private float pass_border;
 }
