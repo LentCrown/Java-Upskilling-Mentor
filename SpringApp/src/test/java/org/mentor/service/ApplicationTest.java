bad489dff64d21d8ac65e28a921f9d92cd28bd04
LentCrown <lickly@yandex.ru>
2021-05-20 12:53:31 +0300

    introduced some tests

--- SpringApp/src/test/java/org/mentor/service/ApplicationTest.java
+++ SpringApp/src/test/java/org/mentor/service/ApplicationTest.java
@@ -1,57 +1,47 @@
 package org.mentor.service;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
-import org.mentor.config.CSVConfigTest;
-import org.mentor.config.ReportConfigTest;
-import org.mentor.config.SourcePathConfigTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@EnableConfigurationProperties({SourcePathConfigTest.class, ReportConfigTest.class, CSVConfigTest.class})
-class ApplicationTest {
-    private final String SOURCE = "Test.csv";
-    private final float PASS_BORDER = 100;
-    private final char SEPARATOR = '+';
+import org.mentor.config.CSVTestConfig;
+import org.mentor.config.ReportTestConfig;
+import org.mentor.config.SourcePathConfig;
+import org.mentor.config.SourcePathTestConfig;
+import org.mentor.dao.CSVFile;
+import org.mentor.dao.QuestionDAO;
+import org.mentor.entity.Answer;
+import org.mentor.entity.User;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-    @Autowired
-    private SourcePathConfigTest sourcePathConfigTest;
-    @Autowired
-    private ReportConfigTest reportConfigTest;
-    @Autowired
-    private CSVConfigTest csvConfigTest;
+import java.util.Arrays;
 
-    @Test
-    void testSourcePathConfigRead(){
-        String source = sourcePathConfigTest.getSource();
-        Assertions.assertEquals(SOURCE,source, "Error occurred while trying to read source");
-        System.out.println("source=" + source);
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+public class ApplicationTest {
+    @Mock
+    private SourcePathConfig sourcePathConfig;
+    @Mock
+    private QuestionDAO questionDAO;
+    @Mock
+    private User user;
+    private Application application;
+
+    public ApplicationTest(){
+        MockitoAnnotations.openMocks(this);
+        this.application = new Application(sourcePathConfig,questionDAO,user);
     }
 
-    @Test
-    void testReportConfigRead(){
-        float pass_border = reportConfigTest.getPass_border();
-        Assertions.assertEquals(PASS_BORDER,pass_border);
-        System.out.println("pass_border=" + pass_border);
-    }
-
-    @Test
-    void testCSVConfigRead(){
-        char separator = csvConfigTest.getSeparator();
-        boolean ignore_quotations = csvConfigTest.isIgnore_quotations();
-        Assertions.assertEquals(SEPARATOR,separator);
-        Assertions.assertTrue(ignore_quotations);
-        System.out.println("separator=" + separator);
-        System.out.println("ignore_quotations=" + ignore_quotations);
-    }
+    /*@Test
+    public void testRunSuccess(){
+        given(sourcePathConfig.getSource()).willReturn("Example1.csv");
+        given(questionDAO.getAnswers(anyString())).willReturn(Arrays.asList(
+                new Answer(1,"5"),
+                new Answer(2,"Тёплое"),
+                new Answer(3, "Северный ледовитый"),
+                new Answer(4, "109")
+        ));
+    }*/
 
 }
